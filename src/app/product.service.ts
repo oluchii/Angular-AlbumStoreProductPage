e80b5ceba33aea88d6ef0ec0197ae9cfd133ce22
Oluchi Onyekpa <onyekpaoluchi@gmail.com>
2020-10-14 23:19:53 +0100

    Completed Module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
@@ -16,5 +17,7 @@ export class ProductService {
     getAlbum(id: number): Observable<Album> {
       return this._http.get(this._albumUrl).map(response => <Album>response.json());
     }
-
+    getProducts(): Observable<Product[]> {
+      return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+    }
 }
